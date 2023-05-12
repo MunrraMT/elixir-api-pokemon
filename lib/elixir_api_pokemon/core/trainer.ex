e12266e9e4ecbe01ee81e202e85267d290f77413
MunrraMT <dev.armsantos@gmail.com>
2023-05-12 11:10:01 -0400

    create trainer remove_pokemon function

--- lib/elixir_api_pokemon/core/trainer.ex
+++ lib/elixir_api_pokemon/core/trainer.ex
@@ -27,4 +27,31 @@ defmodule ElixirApiPokemon.Core.Trainer do
   def add_pokemon(%__MODULE__{} = trainer, _pokemon_id) do
     {:error, trainer}
   end
+
+  def remove_pokemon(
+        %__MODULE__{number_pokemons: number_pokemons, pokemons: current_pokemons} = trainer,
+        pokemon_index
+      )
+      when is_integer(number_pokemons) and number_pokemons >= 2 and
+             pokemon_index <= number_pokemons and pokemon_index > 1 do
+    removed_pokemon_to_list =
+      current_pokemons
+      |> Map.delete(pokemon_index)
+      |> Map.values()
+
+    cleaned_trainer = %__MODULE__{trainer | number_pokemons: 0, pokemons: %{}}
+
+    new_trainer =
+      removed_pokemon_to_list
+      |> Enum.reduce(cleaned_trainer, fn pokemon_id, cleaned_trainer ->
+        {:ok, updated_trainer} = __MODULE__.add_pokemon(cleaned_trainer, pokemon_id)
+        updated_trainer
+      end)
+
+    {:ok, new_trainer}
+  end
+
+  def remove_pokemon(%__MODULE__{} = trainer, _pokemon_id) do
+    {:error, trainer}
+  end
 end
