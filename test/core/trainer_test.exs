bc5950b27a51b60788ec8b276999ac278ca9a11e
MunrraMT <dev.armsantos@gmail.com>
2023-05-12 22:46:30 -0400

    create ElixirApiPokemon.Core.Trainer test for new, add_pokemon and remove_pokemon

---
+++ test/core/trainer_test.exs
@@ -0,0 +1,130 @@
+defmodule Core.TrainerTest do
+  use ExUnit.Case
+  doctest(ElixirApiPokemon.Core.Trainer)
+
+  alias ElixirApiPokemon.Core.Trainer
+
+  describe "ElixirApiPokemon.Core.Trainer.new/1" do
+    test "should return a new trainer Maria with initial pokemon 4" do
+      expected_response = %Trainer{
+        name: "Maria Belizario",
+        pokemons: [4],
+        number_pokemons: 1
+      }
+
+      assert Trainer.new(%{name: "Maria Belizario", initial_pokemon_id: 4}) ==
+               {:ok, expected_response}
+    end
+
+    test "should return a new trainer Camila with initial pokemon 10" do
+      expected_response = %Trainer{
+        name: "Camila maria",
+        pokemons: [25],
+        number_pokemons: 1
+      }
+
+      assert Trainer.new(%{name: "Camila maria", initial_pokemon_id: 25}) ==
+               {:ok, expected_response}
+    end
+  end
+
+  describe "ElixirApiPokemon.Core.Trainer.add_pokemon/2" do
+    test "should return a trainer Maria with pokemons 4 and 16" do
+      trainer = %Trainer{
+        name: "Maria Belizario",
+        pokemons: [4],
+        number_pokemons: 1
+      }
+
+      expected_response = %Trainer{
+        name: "Maria Belizario",
+        pokemons: [4, 16],
+        number_pokemons: 2
+      }
+
+      assert Trainer.add_pokemon(trainer, 16) == {:ok, expected_response}
+    end
+
+    test "should return a trainer Camila with pokemons 10 and 25" do
+      trainer = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10],
+        number_pokemons: 1
+      }
+
+      expected_response = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10, 25],
+        number_pokemons: 2
+      }
+
+      assert Trainer.add_pokemon(trainer, 25) == {:ok, expected_response}
+    end
+
+    test "should return a error when incorrect arguments is passed, and return trainer without changed" do
+      trainer = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10],
+        number_pokemons: 1
+      }
+
+      assert Trainer.add_pokemon(trainer, "25") == {:error, trainer}
+      assert Trainer.add_pokemon(trainer, "error") == {:error, trainer}
+    end
+  end
+
+  describe "ElixirApiPokemon.Core.Trainer.remove_pokemon/2" do
+    test "should return a trainer Maria with pokemons 4 and without 16" do
+      trainer = %Trainer{
+        name: "Maria Belizario",
+        pokemons: [4, 16],
+        number_pokemons: 2
+      }
+
+      expected_response = %Trainer{
+        name: "Maria Belizario",
+        pokemons: [4],
+        number_pokemons: 1
+      }
+
+      assert Trainer.remove_pokemon(trainer, 16) == {:ok, expected_response}
+    end
+
+    test "should return a trainer Camila with pokemons 10 and without 25" do
+      trainer = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10, 25],
+        number_pokemons: 2
+      }
+
+      expected_response = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10],
+        number_pokemons: 1
+      }
+
+      assert Trainer.remove_pokemon(trainer, 25) == {:ok, expected_response}
+    end
+
+    test "should return a error when pokemon id is not exist inside trainer, and return trainer without changed" do
+      trainer = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10, 20],
+        number_pokemons: 2
+      }
+
+      assert Trainer.remove_pokemon(trainer, 30) == {:error, trainer}
+    end
+
+    test "should return a error when incorrect arguments is passed, and return trainer without changed" do
+      trainer = %Trainer{
+        name: "Camila Maria",
+        pokemons: [10, 20],
+        number_pokemons: 2
+      }
+
+      assert Trainer.remove_pokemon(trainer, "25") == {:error, trainer}
+      assert Trainer.remove_pokemon(trainer, "error") == {:error, trainer}
+    end
+  end
+end
