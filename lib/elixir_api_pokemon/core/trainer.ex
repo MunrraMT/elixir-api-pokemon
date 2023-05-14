349f8d02920cc1d0660626f6785c85ab4366b1cd
MunrraMT <dev.armsantos@gmail.com>
2023-05-13 23:20:51 -0400

    create ElixirApiPokemon.Core.Trainer change_order_pokemon

--- lib/elixir_api_pokemon/core/trainer.ex
+++ lib/elixir_api_pokemon/core/trainer.ex
@@ -48,4 +48,40 @@ defmodule ElixirApiPokemon.Core.Trainer do
   def remove_pokemon(%__MODULE__{} = trainer, _pokemon_id) do
     {:error, trainer}
   end
+
+  def change_order_pokemon(
+        %__MODULE__{number_pokemons: number_pokemons, pokemons: current_pokemons} = trainer,
+        pokemon_origem_index,
+        pokemon_target_index
+      )
+      when number_pokemons > 1 and
+             is_integer(pokemon_origem_index) and
+             pokemon_origem_index >= 0 and
+             pokemon_origem_index <= number_pokemons and
+             is_integer(pokemon_target_index) and
+             pokemon_target_index <= number_pokemons and
+             pokemon_target_index >= 0 do
+    pokemon_origem = Enum.at(current_pokemons, pokemon_origem_index)
+    pokemon_target = Enum.at(current_pokemons, pokemon_target_index)
+
+    new_pokemon_list =
+      current_pokemons
+      |> List.replace_at(pokemon_target_index, pokemon_origem)
+      |> List.replace_at(pokemon_origem_index, pokemon_target)
+
+    new_trainer = %__MODULE__{
+      trainer
+      | pokemons: new_pokemon_list
+    }
+
+    {:ok, new_trainer}
+  end
+
+  def change_order_pokemon(
+        %__MODULE__{} = trainer,
+        _pokemon_origem_index,
+        _pokemon_target_index
+      ) do
+    {:error, trainer}
+  end
 end
