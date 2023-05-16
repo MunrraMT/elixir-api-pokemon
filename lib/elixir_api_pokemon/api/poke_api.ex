78f286581321d90c246a4aec565fb114df6b4879
MunrraMT <dev.armsantos@gmail.com>
2023-05-16 01:31:33 -0400

    create ElixirApiPokemon.Api.PokeApi get_kanto_pokemons/0

---
+++ lib/elixir_api_pokemon/api/poke_api.ex
@@ -0,0 +1,16 @@
+defmodule ElixirApiPokemon.Api.PokeApi do
+  @url_poke_api_base "https://pokeapi.co/api/v2/pokemon"
+  @headers [{"Content-Type", "application/json"}]
+
+  def get_kanto_pokemons() do
+    %HTTPoison.Response{
+      body: body_response
+    } = HTTPoison.get!("#{@url_poke_api_base}?limit=150", @headers)
+
+    %{"results" => results} = Jason.decode!(body_response)
+
+    results
+    |> Enum.map(fn %{"name" => name} -> name end)
+    |> Enum.zip(0..149)
+  end
+end
