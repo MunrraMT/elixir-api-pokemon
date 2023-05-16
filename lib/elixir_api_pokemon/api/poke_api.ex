e99ce5ff9c56e7b0e365721ae4513bacc12e6d7a
MunrraMT <dev.armsantos@gmail.com>
2023-05-16 01:33:03 -0400

    create ElixirApiPokemon.Api.PokeApi get_pokemon_info/1

--- lib/elixir_api_pokemon/api/poke_api.ex
+++ lib/elixir_api_pokemon/api/poke_api.ex
@@ -13,4 +13,36 @@ defmodule ElixirApiPokemon.Api.PokeApi do
     |> Enum.map(fn %{"name" => name} -> name end)
     |> Enum.zip(0..149)
   end
+
+  def get_pokemon_info(pokemon_number) do
+    %HTTPoison.Response{
+      body: body_response
+    } = HTTPoison.get!("#{@url_poke_api_base}/#{pokemon_number}", @headers)
+
+    %{
+      "id" => id,
+      "species" => %{"name" => name},
+      "stats" => [
+        %{"base_stat" => life_points_value},
+        %{"base_stat" => attack_value},
+        %{"base_stat" => defense_value},
+        %{"base_stat" => special_attack_value},
+        %{"base_stat" => special_defense_value},
+        %{"base_stat" => speed_value}
+      ]
+    } = Jason.decode!(body_response)
+
+    %{
+      id: id,
+      name: name,
+      stats: %{
+        life_points: life_points_value,
+        attack: attack_value,
+        defense: defense_value,
+        special_attack: special_attack_value,
+        special_defense: special_defense_value,
+        speed: speed_value
+      }
+    }
+  end
 end
