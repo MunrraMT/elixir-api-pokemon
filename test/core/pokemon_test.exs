6c94f38c0613089b64b6651c33e660a03fe7c04f
MunrraMT <dev.armsantos@gmail.com>
2023-05-16 09:45:36 -0400

    create ElixirApiPokemon.Core.Pokemon attack_weak/1 test

--- test/core/pokemon_test.exs
+++ test/core/pokemon_test.exs
@@ -19,7 +19,27 @@ defmodule Core.PokemonTest do
   end
 
   describe "attack_strong/1" do
-    test "should return damage and precision value if pokemon struct passed by argument" do
+    test "should return damage and precision value based on pokemon stats if pokemon struct passed by argument" do
+      {:ok,
+       %Pokemon{
+         stats: %{
+           attack: attack_value,
+           special_attack: special_attack_value
+         }
+       } = new_pokemon} = Pokemon.new(@pokemon_test_id)
+
+      min_damage = round((attack_value + special_attack_value) / 2 * (40 / 100))
+      min_precision = 10
+
+      {:ok, damage, precision} = Pokemon.attack_strong(new_pokemon)
+
+      assert damage >= min_damage
+      assert precision >= min_precision
+    end
+  end
+
+  describe "attack_weak/1" do
+    test "should return damage and precision value based on pokemon stats if pokemon struct passed by argument" do
       {:ok,
        %Pokemon{
          stats: %{
