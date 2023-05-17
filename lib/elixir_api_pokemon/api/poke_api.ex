129e36d01d35c33013afb5df91051f404686bab2
MunrraMT <dev.armsantos@gmail.com>
2023-05-16 23:31:04 -0400

    refactor ElixirApiPokemon.Api.PokeApi get_pokemon_info/1

--- lib/elixir_api_pokemon/api/poke_api.ex
+++ lib/elixir_api_pokemon/api/poke_api.ex
@@ -8,10 +8,9 @@ defmodule ElixirApiPokemon.Api.PokeApi do
     } = HTTPoison.get!("#{@url_poke_api_base}?limit=150", @headers)
 
     %{"results" => results} = Jason.decode!(body_response)
+    pokemons_name = results |> Enum.map(fn %{"name" => name} -> name end)
 
-    results
-    |> Enum.map(fn %{"name" => name} -> name end)
-    |> Enum.zip(0..149)
+    0..149 |> Enum.zip(pokemons_name) |> Enum.into(%{})
   end
 
   def get_pokemon_info(pokemon_id) do
