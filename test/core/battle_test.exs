2dbb8395e10b59785c8a0b804f2ff60680addd7d
MunrraMT <dev.armsantos@gmail.com>
2023-06-14 23:32:10 -0400

    create battle test for new module

--- test/core/battle_test.exs
+++ test/core/battle_test.exs
@@ -1,85 +1,73 @@
 defmodule Core.BattleTest do
   use ExUnit.Case, async: true
 
-  alias ElixirApiPokemon.Core.Pokemon
-  alias ElixirApiPokemon.Core.Battle
-  alias ElixirApiPokemon.Core.Trainer
+  alias ElixirApiPokemon.Core.{Pokemon, Battle, Trainer}
 
   doctest(Battle)
 
   @trainer_test %Trainer{
     name: "Maria Belizario",
-    pokemons: %{0 => 4, 1 => 0, 2 => 5},
+    pokemons: %{0 => 4, 1 => 1, 2 => 5},
     number_pokemons: 3
   }
 
-  @trainer_test_first_pokemon %ElixirApiPokemon.Core.Pokemon{
-    id: 4,
-    name: "charmander",
-    stats: %{
-      attack: 52,
-      defense: 43,
-      life_points: 39,
-      special_attack: 60,
-      special_defense: 50,
-      speed: 65
-    }
+  @trainer_battle_test %Trainer{
+    name: "Maria Belizario",
+    pokemons: %{
+      0 => %ElixirApiPokemon.Core.Pokemon{
+        id: 4,
+        name: "charmander",
+        stats: %{
+          attack: 52,
+          defense: 43,
+          life_points: 39,
+          special_attack: 60,
+          special_defense: 50,
+          speed: 65
+        }
+      },
+      1 => %ElixirApiPokemon.Core.Pokemon{
+        id: 1,
+        name: "bulbasaur",
+        stats: %{
+          attack: 49,
+          defense: 49,
+          life_points: 45,
+          special_attack: 65,
+          special_defense: 65,
+          speed: 45
+        }
+      },
+      2 => %ElixirApiPokemon.Core.Pokemon{
+        id: 5,
+        name: "charmeleon",
+        stats: %{
+          attack: 64,
+          defense: 58,
+          life_points: 58,
+          special_attack: 80,
+          special_defense: 65,
+          speed: 80
+        }
+      }
+    },
+    number_pokemons: 3
   }
 
-  describe "build_wild_pokemon_battle/1" do
-    test "should return initial Battle struct with the trainer's first pokemon passed by argument and one random wild pokemon" do
+  describe "build/2" do
+    test "should returns battle struct if recebe player trainer and computer trainer valid arguments" do
+      {:ok, player_trainer} = Trainer.build("Maria Belizario", 1)
+      {:ok, computer_trainer} = Trainer.get_random_trainer(1)
+
       {:ok,
-       %Battle{
-         player: player,
-         player_pokemon: player_pokemon,
-         computer: %computer_struct{} = computer,
-         computer_pokemon: computer_pokemon,
-         status: status
-       }} = Battle.build_wild_pokemon_battle(@trainer_test)
+       %battle_struct_name{
+         player: player_trainer_battle,
+         computer: computer_trainer_battle
+       } = _battle_struct} = Battle.build(player_trainer, computer_trainer)
 
-      assert player == @trainer_test
-      assert player_pokemon == @trainer_test_first_pokemon
-      assert computer_struct == Pokemon
-      assert computer == computer_pokemon
-      assert status == :started
-    end
-  end
-
-  describe "build_trainer_pokemon_battle/2" do
-    test "should return initial Battle struct with the trainer's first pokemon passed by argument and random computer trainer with your first pokemon and other 3 pokemons" do
-      {:ok,
-       %Battle{
-         player: player,
-         player_pokemon: player_pokemon,
-         computer: %computer_struct{number_pokemons: computer_number_pokemons} = _computer,
-         computer_pokemon: %computer_pokemon_struct{} = _computer_pokemon,
-         status: status
-       }} = Battle.build_trainer_pokemon_battle(@trainer_test, 4)
-
-      assert player == @trainer_test
-      assert player_pokemon == @trainer_test_first_pokemon
-      assert computer_struct == Trainer
-      assert computer_pokemon_struct == Pokemon
-      assert status == :started
-      assert computer_number_pokemons == 4
-    end
-
-    test "should return initial Battle struct with the trainer's first pokemon passed by argument and random computer trainer with your first pokemon and other 5 pokemons" do
-      {:ok,
-       %Battle{
-         player: player,
-         player_pokemon: player_pokemon,
-         computer: %computer_struct{number_pokemons: computer_number_pokemons} = _computer,
-         computer_pokemon: %computer_pokemon_struct{} = _computer_pokemon,
-         status: status
-       }} = Battle.build_trainer_pokemon_battle(@trainer_test, 6)
-
-      assert player == @trainer_test
-      assert player_pokemon == @trainer_test_first_pokemon
-      assert computer_struct == Trainer
-      assert computer_pokemon_struct == Pokemon
-      assert status == :started
-      assert computer_number_pokemons == 6
+      assert battle_struct_name == Battle
+      assert player_trainer_battle.name == player_trainer.name
+      assert computer_trainer_battle.name == computer_trainer.name
     end
   end
 end
