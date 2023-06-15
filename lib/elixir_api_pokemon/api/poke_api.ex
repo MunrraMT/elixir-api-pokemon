15d16860e2218243c213cde2e4e79e184c9ab8c5
MunrraMT <dev.armsantos@gmail.com>
2023-06-14 23:30:22 -0400

    refactor pokeapi change pokemon id

--- lib/elixir_api_pokemon/api/poke_api.ex
+++ lib/elixir_api_pokemon/api/poke_api.ex
@@ -10,10 +10,10 @@ defmodule ElixirApiPokemon.Api.PokeApi do
     %{"results" => results} = Jason.decode!(body_response)
     pokemons_name = results |> Enum.map(fn %{"name" => name} -> name end)
 
-    0..149 |> Enum.zip(pokemons_name) |> Enum.into(%{})
+    1..150 |> Enum.zip(pokemons_name) |> Enum.into(%{})
   end
 
-  def get_pokemon_info(pokemon_id) do
+  def get_pokemon_info(pokemon_id) when is_integer(pokemon_id) and pokemon_id >= 1 do
     %HTTPoison.Response{
       body: body_response
     } = HTTPoison.get!("#{@url_poke_api_base}/#{pokemon_id}", @headers)
